57c455310397429a5393d19dad6058c3e81caae0
SushyDev <xsushy@hotmail.com>
2024-02-10 16:12:24 +0100

    improve config

--- config/main.go
+++ config/main.go
@@ -2,13 +2,99 @@ package config
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v3"
 	"io"
 	"os"
 	"reflect"
+	"strconv"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 var config Config
+
+func setFieldDefault(field reflect.Value, fieldType reflect.StructField) {
+	defaultTag, ok := fieldType.Tag.Lookup("default")
+	if !ok || !field.CanSet() {
+		return
+	}
+
+	switch field.Kind() {
+	case reflect.String:
+		if field.String() == "" {
+			field.SetString(defaultTag)
+		}
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		if field.Int() == 0 {
+			intValue, err := strconv.ParseInt(defaultTag, 10, 64)
+			if err == nil {
+				field.SetInt(intValue)
+			}
+		}
+	case reflect.Bool:
+		if !field.Bool() {
+			boolValue, err := strconv.ParseBool(defaultTag)
+			if err == nil {
+				field.SetBool(boolValue)
+			}
+		}
+	case reflect.Slice:
+		if field.Len() == 0 {
+			input := strings.Trim(defaultTag, "[]")
+			parts := strings.Split(input, ",")
+
+			for _, part := range parts {
+				part = strings.TrimSpace(part)
+				field.Set(reflect.Append(field, reflect.ValueOf(part)))
+			}
+		}
+	}
+}
+
+func hasInitTag(fieldType reflect.StructField) bool {
+	_, ok := fieldType.Tag.Lookup("init")
+
+	return ok
+}
+
+func setDefaultsRecursive(value reflect.Value) {
+	if value.Kind() == reflect.Ptr {
+		value = value.Elem()
+	}
+
+	if value.Kind() != reflect.Struct {
+		return
+	}
+
+	for i := 0; i < value.NumField(); i++ {
+		field := value.Field(i)
+		typ := value.Type().Field(i)
+
+		if field.Kind() == reflect.Struct {
+			setDefaultsRecursive(field)
+		} else if field.Kind() == reflect.Ptr && field.Type().Elem().Kind() == reflect.Struct {
+			if field.IsNil() && hasInitTag(typ) {
+				field.Set(reflect.New(field.Type().Elem()))
+			}
+
+			if !field.IsNil() {
+				setDefaultsRecursive(field)
+			}
+		} else {
+			setFieldDefault(field, typ)
+		}
+	}
+}
+
+func setDefaults(v interface{}) {
+	value := reflect.ValueOf(v)
+
+	if value.Kind() != reflect.Ptr || value.IsNil() {
+		panic("SetDefaults: expected a non-nil pointer")
+	}
+
+	setDefaultsRecursive(value.Elem())
+}
 
 func GetConfig() Config {
 	if !reflect.DeepEqual(config, Config{}) {
@@ -32,6 +118,14 @@ func GetConfig() Config {
 		fmt.Printf("Error unmarshalling config file: %v\n", err)
 		panic(err)
 	}
+
+	err = configFile.Close()
+	if err != nil {
+		fmt.Printf("Error closing config file: %v\n", err)
+		panic(err)
+	}
+
+	setDefaults(&config)
 
 	return config
 }
--- config/structs.go
+++ config/structs.go
@@ -5,29 +5,40 @@ type RealDebrid struct {
 }
 
 type Sonarr struct {
-	Host  string `yaml:"host"`
+	Host  string `yaml:"host" default:"http://localhost:8989"`
 	Token string `yaml:"token"`
 }
 
 type Radarr struct {
-	Host  string `yaml:"host"`
+	Host  string `yaml:"host" default:"http://localhost:7878"`
 	Token string `yaml:"token"`
 }
 
 type Zurg struct {
-	Host string `yaml:"host"`
+	Host string `yaml:"host" default:"http://localhost:9999"`
+}
+
+type QDebrid struct {
+	Host string `yaml:"host" default:""`
+	Port int    `yaml:"port" default:"8080"`
+
+	ResponseCacheTTL string `yaml:"response_cache_ttl" default:"5m"`
+
+	CategoryName string `yaml:"category_name" default:"qdebrid"`
+	SavePath     string `yaml:"save_path" default:"/mnt/zurg/__all__"`
+
+	ValidatePaths bool `yaml:"validate_paths" default:"true"`
+
+	AllowUncached    bool     `yaml:"allow_uncached" default:"false"`
+	AllowedFileTypes []string `yaml:"allowed_file_types" default:"[mkv, mp4]"`
 }
 
 type Settings struct {
-	Host          string     `yaml:"host"` // Optional
-	Port          int        `yaml:"port"` // 8080 by default
-	CategoryName  string     `yaml:"category_name"` // Download Client category name
-	SavePath      string     `yaml:"save_path"` // Debrid mount path (/mnt/zurg/__all__)
-	ValidatePaths bool       `yaml:"validate_paths"` // Ping zurg to check if path exists
-	RealDebrid    RealDebrid `yaml:"real_debrid"`
-	Sonarr        Sonarr     `yaml:"sonarr"`
-	Radarr        Radarr     `yaml:"radarr"`
-	Zurg          Zurg       `yaml:"zurg"`
+	QDebrid    QDebrid    `yaml:"qdebrid"`
+	RealDebrid RealDebrid `yaml:"real_debrid"`
+	Sonarr     Sonarr     `yaml:"sonarr"`
+	Radarr     Radarr     `yaml:"radarr"`
+	Zurg       Zurg       `yaml:"zurg"`
 }
 
 type Config struct {
